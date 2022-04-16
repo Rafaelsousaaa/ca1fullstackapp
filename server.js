b5af3307896167a152275f78464879efdac97b49
Rafaelsousaaa <103071697+Rafaelsousaaa@users.noreply.github.com>
2022-04-16 18:00:43 +0500

    Added Main screen redirect route and Backend API

--- server.js
+++ server.js
@@ -39,4 +39,35 @@ app.use(express.static('assets'));
 app.listen(5000, () => {
     console.log('Port 5000')
 })
+// ****************************************** CONSUMING GET REQUEST FETCHING ALL BLOGS IN UI******************************************
 
+//get all blog list
+app.get('/', (req, res) => {
+    axios.get('http://localhost:5000/blog/list')
+        .then(function(response) {
+            // return index and passs blogs as argument (list)
+            res.render('index', { blogs: response.data });
+        })
+        .catch(err => {
+            res.send(err);
+        })
+
+})
+
+// ****************************************** API TO GET ALL BLOGS FROM MONGODB ******************************************
+
+app.get('/blog/list', (req, res) => {
+
+   // fetching All the blogs from database
+   Blog.find()
+   .then(data => {
+       res.send(data)
+   })
+   .catch(err => {
+       res.status(500).send({
+       message:
+           err.message || "Some error occurred while retrieving Blogs."
+       });
+   });
+
+})
