3d4749f7de9bde1561aec26ebd1b86932b55364d
Rafaelsousaaa <103071697+Rafaelsousaaa@users.noreply.github.com>
2022-05-17 12:10:31 +0100

    Update index.js

--- models/index.js
+++ models/index.js
@@ -3,7 +3,7 @@ mongoose.Promise = global.Promise;
 
 const db = {};
 db.mongoose = mongoose;
-db.url = "mongodb+srv://Rafaelsousaa:Rafaelsousaa@cluster0.wa6io.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+db.url = "";
 db.Blogs = require("./blogModel.js")(mongoose);
 
 module.exports = db;
