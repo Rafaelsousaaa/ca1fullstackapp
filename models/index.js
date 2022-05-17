968c4d5175aabc741d77bae1952958c5e2418935
Rafaelsousaaa <103071697+Rafaelsousaaa@users.noreply.github.com>
2022-05-17 20:28:22 +0500

    fetching mongodb from env variable

--- models/index.js
+++ models/index.js
@@ -1,9 +1,10 @@
 const mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
+require('dotenv').config();
 
 const db = {};
 db.mongoose = mongoose;
-db.url = "";
+db.url =process.env.MONGODB; ;
 db.Blogs = require("./blogModel.js")(mongoose);
 
 module.exports = db;
